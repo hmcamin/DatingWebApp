214d85784a06c70009e97837e234762adc601180
h_mcamin@yahoo.com <h_mcamin@yahoo.com>
2022-09-13 11:52:28 +0430

    create login and user exists repository methods

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -13,7 +13,7 @@ namespace DatingApp.API.Data
         {
             _context = context;
         }
-        public Task<User> Register(User user, string password) {
+        public async Task<User> Register(User user, string password) {
             byte[] passwordHash, passwordSalt;
             CreatePasswordHash(password, out passwordHash, out passwordSalt);
 
@@ -25,20 +25,41 @@ namespace DatingApp.API.Data
 
             return user;
         }
+        public async Task<User> Login(string username, string password){
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+
+            if(user == null)
+                return null;
+
+            if(VerifyPasswordHash(password, user.passwordHash, user.passwordSalt))
+                return null;
+
+            return user;
+        }
+
+        public async Task<bool> UserExists(string username){
+            if(await _context.Users.AnyAsync(x => x.Username == username)) return true;
+            
+            return false;
+        }
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt){
-            using(var hmac = new System.Security.Cryptography.HMACSHA512)
+            using(var hmac = new System.Security.Cryptography.HMACSHA512())
             {
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.computeHash(System.Text.Encoding.UTF8.GetBytes(password));
             }
             
         }
-        public Task<User> Login(string username, string password){
-
-        }
-
-        public Task<bool> UserExists(string username){
-
+        private void VerifyPasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt){
+            using(var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.computeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for(i=0; i< computedHash.length; i++){
+                    if(computedHash[i] != passwordHash[i]) return false;
+                }
+            }
+            return true;
+            
         }
     }
 }
