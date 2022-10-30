9f15edbbe0b99a313b3373fd7c25b0291295da83
Hossein <h_mcamin@yahoo.com>
2022-10-30 23:01:20 +0330

    Add validation for user register

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,13 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+		[Required]
         public string Username { get; set; }
-        public string Password { get; set; }
+        
+		[Required]
+		[StringLength(8,MininumLength = 4, ErrorMessage = "You must specify password bewteen 4 and 8 charecters")]
+		public string Password { get; set; }
     }
 }
