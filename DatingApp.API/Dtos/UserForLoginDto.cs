459ccd90057572d6c9253ff91aa626fc903a3209
Hossein <h_mcamin@yahoo.com>
2022-10-31 00:04:08 +0330

    update userForLoginDto validations

--- DatingApp.API/Dtos/UserForLoginDto.cs
+++ DatingApp.API/Dtos/UserForLoginDto.cs
@@ -2,13 +2,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace DatingApp.API.Dtos
 {
-    public class UserForRegisterDto
+    public class UserForLoginDto
     {
-		[Required]
         public string Username { get; set; }
         
-		[Required]
-		[StringLength(8,MininumLength = 4, ErrorMessage = "You must specify password bewteen 4 and 8 charecters")]
 		public string Password { get; set; }
     }
 }
