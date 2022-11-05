5089467c29a696cc4ee005003bcd4cdab428cc2f
Hossein <h_mcamin@yahoo.com>
2022-11-05 22:53:50 +0330

    setting up the global exception handler in the API

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -1,20 +1,33 @@
 ﻿using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.API.Data;
+using DatingApp.API.Helpers;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace DatingApp.API
 {
@@ -55,6 +68,17 @@ namespace DatingApp.API
             }
             else
             {
+				app.UseExceptionHandler(builder => {
+					builder.Run(async context => {
+						context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+						
+						var error = context.Features.Get<IExceptionHandlerFeature>();
+						if(error != null) {
+							context.Response.AddApplicationError(error.Error.Message);
+							await context.Response.WriteAsync(error.Error.Message);
+						}
+					});
+				});
                 // app.UseHsts();
             }
 
@@ -65,6 +89,15 @@ namespace DatingApp.API
         }
     }
 }
+
+
+
+
+
+
+
+
+
 
 
 
