012a920887c56869b3bb9a75101476ff8c76197f
Hossein <h_mcamin@yahoo.com>
2022-11-06 20:07:42 +0330

    add alertify to SPA

---
+++ DatingApp-SPA/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { AlertifyService } from './_services/alertify.service';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+	  NavComponent,
+	  HomeComponent,
+	  RegisterComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule,
+	  FormsModule
+   ],
+   providers: [
+	  AuthService,
+	  ErrorInterceptorProviderو
+	  AlertifyService
+   ],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
