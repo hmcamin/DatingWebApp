1f3827c2649660345ffe188a849b49fa14b5f835
Hossein <h_mcamin@yahoo.com>
2022-11-19 23:53:42 +0330

    add routes to SPA

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import { BsDropdownModule } from 'ngx-bootstrap'
+import { BsDropdownModule } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -11,6 +12,10 @@ import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
+import { appRoutes } from '@./routes';
 
 @NgModule({
    declarations: [
@@ -23,7 +28,8 @@ import { AlertifyService } from './_services/alertify.service';
       BrowserModule,
       HttpClientModule,
 	  FormsModule,
-	  BsDropdownModule.forRoot()
+	  BsDropdownModule.forRoot(),
+	  RouterModule.forRoot(appRoutes)
    ],
    providers: [
 	  AuthService,
