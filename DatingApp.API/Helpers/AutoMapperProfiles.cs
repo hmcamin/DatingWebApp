47342c24e2ee815af5dfc280e9d1de6a591cd08e
Hossein <h_mcamin@yahoo.com>
2022-11-29 19:23:13 +0330

    extend automapper profile
    create functions for claculate age and photoUrl for usersDto

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
@@ -7,9 +8,24 @@ namespace DatingApp.API.Helpers
     public class AutoMapperProfiles : Profile
     {
         public AutoMapperProfiles() {
-            CreateMap<User, UserForListDto>();
-            CreateMap<User, UserForDetailedDto>();
+            CreateMap<User, UserForListDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => {
+                    opt.MapFrom(scr => scr.Photos.FirstOrDefault( p => p.IsMain).Url);
+                })
+                .ForMember(dest => dest.Age, opt => {
+                    opt.ResolveUsing( d => d.DateOfBirth.CalculateAge());
+                })           
+            ;
+            CreateMap<User, UserForDetailedDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => {
+                    opt.MapFrom(scr => scr.Photos.FirstOrDefault( p => p.IsMain).Url);
+                })           
+                .ForMember(dest => dest.Age, opt => {
+                    opt.ResolveUsing( d => d.DateOfBirth.CalculateAge());
+                })           
+            ;
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
