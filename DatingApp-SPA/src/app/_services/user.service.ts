efbf851c1c6244b1484411809e837626033d3789
Hossein <h_mcamin@yahoo.com>
2022-12-02 23:51:26 +0330

    add getUsers functions to user service

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,  HttpParams } from '@angular/common/http';
+import { HttpClient,  HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -8,6 +8,11 @@ import { User } from '../_models/user';
 import { PaginatedResult } from '../_models/pagination';
 import { Message } from '../_models/message';
 
+const httpOptions = {
+	headers: new HttpHeaders({
+		'Authorization': 'Bearer ' + localStorage.getItem('token')
+	})
+};
 
 @Injectable({
   providedIn: 'root'
@@ -16,4 +21,12 @@ export class UserService {
   baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl + 'users', httpOptions);
+  }
+
+  getUser(id): Observable<User> {
+    return this.http.get<User>(this.baseUrl + 'users/' + id , httpOptions);
+  }
 }
