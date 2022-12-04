923cdc2a267db0b08ee72a535f6e587a9dbd340a
Hossein <h_mcamin@yahoo.com>
2022-12-04 22:35:38 +0330

    add loadUsers function to memberList page

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { User } from '../../_models/user';
+import { Pagination, PaginatedResult } from '../../_models/pagination';
+import { UserService } from '../../_services/user.service';
+import { AlertifyService } from '../../_services/alertify.service';
 
 @Component({
   selector: 'app-member-list',
@@ -6,5 +12,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent {
-  
+	users: User[];
+    constructor(
+		private userService: UserService,
+		private alertifyService: AlertifyService) { }
+
+	ngOnInit() {
+		this.loadUsers();
+	});
+	loadUsers(){
+		this.userService.getUsers().subscribe((users: User[]) =>{
+			this.users = users;
+		}, error => {
+			this.alertify.error(error);
+		});
+	}
 }
