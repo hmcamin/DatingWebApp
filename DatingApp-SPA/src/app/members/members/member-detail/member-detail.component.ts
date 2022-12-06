926c1d0bf77714392c8a7b60a2aabcbd1cdebbac
Hossein <h_mcamin@yahoo.com>
2022-12-06 23:03:25 +0330

    add member detail component

---
+++ DatingApp-SPA/src/app/members/members/member-detail/member-detail.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+import { TabsetComponent } from 'ngx-bootstrap';
+
+import { User } from '../../_models/user';
+
+@Component({
+  selector: 'app-member-detail',
+  templateUrl: './member-detail.component.html',
+  styleUrls: ['./member-detail.component.css']
+})
+export class MemberDetailComponent implements OnInit {
+  user: User;
+  constructor(
+    private userService: UserService,
+	private alertify: AlertifyService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    this.loadUser();
+  }
+
+  loadUser() {
+	  this.userService.getUser(+this.route.snapshot.params['id']).subscribe((user: User) => {
+		  this.user = user;
+	  }, error =>{
+		  this.alertify.error(error);
+	  });
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
