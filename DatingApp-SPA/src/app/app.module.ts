f7ae12f9feca31f1f8e4d7541ade0336fd383d86
Hossein <h_mcamin@yahoo.com>
2022-12-07 23:35:28 +0330

    add TabsModule to appModule

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import { BsDropdownModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 
@@ -21,6 +21,8 @@ import { AuthGuard } from './_guards/auth.guard'
 import { UserService } from './_services/user.service'
 import { MessagesComponent } from './messages/messages.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+
 
 export function tokenGetter(){
 	return localStorage.getItem('token');
@@ -35,13 +37,15 @@ export function tokenGetter(){
 	  MemberListComponent,
       ListsComponent,
       MessagesComponent,
-      MemberCardComponent
+      MemberCardComponent,
+	  MemberDetailComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
 	  FormsModule,
 	  BsDropdownModule.forRoot(),
+	  TabsModule.forRoot(),
 	  RouterModule.forRoot(appRoutes),
 	  JwtModule.forRoot({
 		  config: {
