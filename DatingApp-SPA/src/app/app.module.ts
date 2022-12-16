08debd5a418c14098411f486c2c8f7373fd050bc
Hossein <h_mcamin@yahoo.com>
2022-12-16 23:54:40 +0330

    add member list resolver to routes and appModule

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
 
 
 export function tokenGetter(){
@@ -62,7 +63,8 @@ export function tokenGetter(){
 	  AlertifyService,
 	  AuthGuard,
 	  UserService,
-	  MemberDetailResolver
+	  MemberDetailResolver,
+	  MemberListResolver
    ],
    bootstrap: [
       AppComponent
