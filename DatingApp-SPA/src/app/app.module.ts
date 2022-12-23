48f223448d22102c0e149a293cb39bd2da7df403
Hossein <h_mcamin@yahoo.com>
2022-12-23 20:21:13 +0330

    add member edit to appModule and routes

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { UserService } from './_services/user.service'
 import { MessagesComponent } from './messages/messages.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 
@@ -41,7 +42,8 @@ export function tokenGetter(){
       ListsComponent,
       MessagesComponent,
       MemberCardComponent,
-	  MemberDetailComponent
+	  MemberDetailComponent,
+	  MemberEditComponent
    ],
    imports: [
       BrowserModule,
--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -5,6 +5,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard'
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 
@@ -20,6 +21,7 @@ export const appRoutes: Routes = [
 		},
 		{ path: 'members/:id', component: MemberDetailComponent,
 			resolve: {user: MemberDetailResolver}},
+		{ path: 'member/edit', component: MemberEditComponent },
 		{ path: 'messages', component: MessagesComponent },
 		{ path: 'lists', component: ListsComponent },
 		]
