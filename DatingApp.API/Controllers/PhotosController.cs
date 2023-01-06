86001dbcb38ae0def7337a56aeaef7976521eb1d
Hossein <h_mcamin@yahoo.com>
2023-01-06 19:20:37 +0330

    add upload_photo_for_user to photo controller

--- DatingApp.API/Controllers/PhotosController.cs
+++ DatingApp.API/Controllers/PhotosController.cs
@@ -36,5 +36,49 @@ namespace DatingApp.API.Controllers
             );
             _cloudinary = new Cloudinary(acc);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPhotoForUser(int userId,
+            PhotoForCreationDto photoForCreationDto) 
+        {
+            if(userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value)) 
+                return Unauthorized();
+
+            var userFromRepo = await _repo.GetUser(userId, true);
+
+            var file = photoForCreationDto.File;
+            if (file ==null || file.Length ==0) {
+                return BadRequest("Photo sent is not valid");
+            }
+
+            var uploadResult = new ImageUploadResult();
+            using (var stream = file.OpenReadStream())
+            {
+                var uploadParams = new ImageUploadParams() 
+                {
+                    File = new FileDescription(file.Name, stream),
+                    Transformation = new Transformation().Width(500)
+                        .Height(500).Crop("fill").Gravity("face")
+                };
+                uploadResult = _cloudinary.Upload(uploadParams);
+
+            }
+
+            photoForCreationDto.Url = uploadResult.Uri.ToString();
+            photoForCreationDto.PublicId = uploadResult.PublicId;
+
+            var photo = _mapper.Map<Photo>(photoForCreationDto);
+
+            if (!userFromRepo.Photos.Any(u => u.IsMain)) {
+                photo.IsMain = true;
+            }
+
+            userFromRepo.Photos.Add(photo);
+
+            if (await _repo.SaveAll()) {
+                return Ok();
+            }
+            return BadRequest("Could not add the photo");
+        }
 	}
 }
